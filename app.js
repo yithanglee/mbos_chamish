0fec101d34f113043a587f9b8f223c1fb82d3975
Damien <yithanglee@gmail.com>
2024-09-24 12:29:29 +0800

    typo

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ app.post('/send-inquiry', async (req, res) => {
     }
 
 
-    const ip = req.headers.get('CF-Connecting-IP');
+    const ip = req.headers['cf-connecting-ip'];
 
     // Send the request with form-encoded data
     const result = await fetch(url, {
