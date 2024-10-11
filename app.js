1cce2e432b5712ddcad4c88698997e4ff3b35b41
Damien <yithanglee@gmail.com>
2024-10-11 08:44:00 +0800

    increase rate request

--- app.js
+++ app.js
@@ -80,8 +80,8 @@ app.use(
 // Set up rate limiter: maximum of twenty requests per minute
 const RateLimit = require("express-rate-limit");
 const limiter = RateLimit({
-  windowMs: 1 * 60 * 1000, // 5 minute
-  max: 100,
+  windowMs: 1 * 60 * 5000, // 5 minute
+  max: 500,
 });
 // Apply rate limiter to all requests
 app.use(limiter);
